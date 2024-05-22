1340230dccae783efe7a5d8e58260707b9b67b48
SubhanWasif <subhanwasif21@gmail.com>
2024-05-22 23:41:38 +0500

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/data", async (req, res) => {
   try {
